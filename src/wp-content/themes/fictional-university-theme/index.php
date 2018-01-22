c5ce4b133b89d5568fbca34d0187d82731b4fba5
Lazaro A. S. Castro <lazaro.castro@gmail.com>
2018-01-22 14:35:54 -0200

    Created my first theme and tested functions and arrays

---
+++ src/wp-content/themes/fictional-university-theme/index.php
@@ -0,0 +1,12 @@
+<?php
+  $names = array('Brad', 'Lazaro', 'Avelino', 'Santos', 'Castro');
+
+  $count = 0;
+
+  while($count < count($names)) {
+    echo "<p>Hi, my name is $names[$count]</p>";
+    $count++;
+  }
+?>
+
+
